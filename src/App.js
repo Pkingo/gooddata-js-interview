eedf7ef6f4feb2f145d0b762e45384f026f1bbe4
Philip Andreas Kingo <philipandreaskingo@hotmail.com>
2020-02-29 12:17:25 +0100

    Fixed dropdown

--- src/App.js
+++ src/App.js
@@ -4,102 +4,108 @@ import React, { Component } from 'react';
 import '@gooddata/react-components/styles/css/main.css';
 
 import { ColumnChart } from '@gooddata/react-components';
+import moment from 'moment';
 
 const grossProfitMeasure = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/6877';
 const dateAttributeInMonths = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2142';
 const dateAttribute = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2180';
 
 class App extends Component {
+	constructor(props) {
+    super(props)
+		this.state = {
+      projectId: 'xms7ga4tf3g3nzucd8380o2bev8oeknp',
+			filters: [this.getMonthFilter(0)],
+			measures: this.getMeasures(),
+			viewBy: this.getViewBy(),
+		};
+	}
 
-    getMonthFilter() {
-        return {
-            absoluteDateFilter: {
-                dataSet: {
-                    uri: dateAttribute
-                },
-                from: '2016-01-01',
-                to: '2016-01-31'
-            }
+getMonthFilter(month) {
+  const date = new Date(2016, month + 1, 0)
+  const from = moment(date).startOf('month').format('YYYY-MM-DD');
+  const to   = moment(date).endOf('month').format('YYYY-MM-DD');
+	return {
+		absoluteDateFilter: {
+			dataSet: {
+				uri: dateAttribute
+			},
+			from,
+			to
+		}
+	}
+}
 
-        }
-    }
+	getMeasures() {
+		return [{
+			measure: {
+				localIdentifier: 'm1',
+					definition: {
+						measureDefinition: {
+							item: {
+								uri: grossProfitMeasure
+							}
+						}
+					},
+					alias: '$ Gross Profit'
+				}
+			}
+		]
+	}
 
-    getMeasures() {
-        return [
-            {
-                measure: {
-                    localIdentifier: 'm1',
-                    definition: {
-                        measureDefinition: {
-                            item: {
-                                uri: grossProfitMeasure
-                            }
-                        }
-                    },
-                    alias: '$ Gross Profit'
-                }
-            }
-        ]
-    }
+	getViewBy() {
+		return {
+			visualizationAttribute: {
+				displayForm: {
+					uri: dateAttributeInMonths
+				},
+				localIdentifier: 'a1'
+			}
+		}
+	}
 
-    getViewBy() {
-        return {
-            visualizationAttribute:
-            {
-                displayForm: {
-                    uri: dateAttributeInMonths
-                },
-                localIdentifier: 'a1'
-            }
-        }
-    }
+	renderDropdownOption(month, value) {
+		return <option key={value} value={value}>{month}</option>
+	}
+		
+	onDropdownSelect = (event) => {
+		this.setState({filters: [this.getMonthFilter(event.currentTarget.value)]});
+	}
 
-    renderDropdown() {
-        return (
-            <select defaultValue="1">
-                <option value="1">January</option>
-                <option value="2">February</option>
-                <option value="3">March</option>
-                <option value="4">April</option>
-                <option value="5">May</option>
-                <option value="6">June</option>
-                <option value="7">July</option>
-                <option value="8">August</option>
-                <option value="9">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-            </select>
-        )
-    }
+	renderDropdown() {
+		const options = moment.months().map(this.renderDropdownOption);
+		return <select onChange={this.onDropdownSelect} defaultValue="0">{options}</select>;
+	}
 
-    render() {
-        const projectId = 'xms7ga4tf3g3nzucd8380o2bev8oeknp';
-        const filters = [this.getMonthFilter()];
-        const measures = this.getMeasures();
-        const viewBy = this.getViewBy();
+	render() {
+		const {
+			projectId,
+			filters,
+			measures,
+			viewBy,
+		} = this.state;
 
-        return (
-            <div className="App">
-                <h1>$ Gross Profit in month {this.renderDropdown()} 2016</h1>
-                <div>
-                    <ColumnChart
-                        measures={measures}
-                        filters={filters}
-                        projectId={projectId}
-                    />
-                </div>
-                <h1>$ Gross Profit - All months</h1>
-                <div>
-                    <ColumnChart
-                        measures={measures}
-                        viewBy={viewBy}
-                        projectId={projectId}
-                    />
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div className="App">
+				<h1>$ Gross Profit in month {this.renderDropdown()} 2016</h1>
+				<div>
+					<ColumnChart
+						measures={measures}
+						filters={filters}
+						projectId={projectId}
+					/>
+				</div>
+				<h1>$ Gross Profit - All months</h1>
+				<div>
+					<ColumnChart
+						measures={measures}
+						viewBy={viewBy}
+						projectId={projectId}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
